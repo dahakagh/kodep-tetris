d2dfa3a1c1ad7b207060158735ab74e969572691
dahakagh <artem.marshev@kodep.ru>
2024-05-25 13:24:26 +0300

    feat: refactor tetris

--- src/components/Cell/index.tsx
+++ src/components/Cell/index.tsx
@@ -1,23 +1,26 @@
-import { ReactElement, memo } from "react";
+import { ReactElement, memo, CSSProperties } from "react";
 import { SHAPES } from "../../constants";
 
 interface ICellProps {
   type: string | number;
 }
 
-const MemoizedCell = memo(function Cell({ type }: ICellProps): ReactElement {
-  return (
-    <div
-      style={{
-        background: `rgba(${SHAPES[type].color}, 0.8)`,
-        border: `${type === 0 ? "0px solid" : "4px solid"}`,
-        borderBottomColor: `rgba(${SHAPES[type].color}, 0.1)`,
-        borderRightColor: `rgba(${SHAPES[type].color}, 1)`,
-        borderTopColor: `rgba(${SHAPES[type].color}, 1)`,
-        borderLeftColor: `rgba(${SHAPES[type].color}, 0.3)`,
-      }}
-    />
-  );
-});
+const getCellStyle = (type: string | number): CSSProperties => {
+  const color = SHAPES[type].color;
+  return {
+    background: `rgba(${color}, 0.8)`,
+    border: `${type === 0 ? "0px solid" : "4px solid"}`,
+    borderBottomColor: `rgba(${color}, 0.1)`,
+    borderRightColor: `rgba(${color}, 1)`,
+    borderTopColor: `rgba(${color}, 1)`,
+    borderLeftColor: `rgba(${color}, 0.3)`,
+  };
+};
+
+const Cell = ({ type }: ICellProps): ReactElement => {
+  return <div style={getCellStyle(type)} />;
+};
+
+const MemoizedCell = memo(Cell);
 
 export { MemoizedCell as Cell };
