845949d3be871bb19ab79ac3fb69de6b1192d61a
dahakagh <artem.marshev@kodep.ru>
2024-05-28 21:58:08 +0300

    refactor: add types

--- src/hooks/useBoard.ts
+++ src/hooks/useBoard.ts
@@ -18,22 +18,21 @@ export const useBoard = (
   useEffect(() => {
     setRowsCleared(0);
 
-    const cleanRows = (newStage: (string | number)[][][]) =>
-      newStage.reduce((acc, row) => {
-        if (row.findIndex((cell) => cell[0] === 0) === -1) {
+    const cleanRows = (newStage: BoardT): BoardT => {
+      const clearedStage = newStage.reduce((acc, row) => {
+        if (row.every((cell) => cell[0] !== 0)) {
           setRowsCleared((prev) => prev + 1);
           acc.unshift(
-            new Array(newStage[0].length).fill([0, CellStatus.CLEAR]) as (
-              | string
-              | number
-            )[][]
+            new Array(newStage[0].length).fill([0, CellStatus.CLEAR])
           );
-          return acc;
+        } else {
+          acc.push(row);
         }
-
-        acc.push(row);
         return acc;
-      }, [] as (string | number)[][][]);
+      }, [] as BoardT);
+
+      return clearedStage;
+    };
 
     const updateBoard = (prevStage: (string | number)[][][]) => {
       const newStage = prevStage.map((row) =>
@@ -61,7 +60,7 @@ export const useBoard = (
       return newStage;
     };
 
-    setBoard((prev: (string | number)[][][]) => updateBoard(prev));
+    setBoard((prev: BoardT) => updateBoard(prev));
   }, [figure, resetFigure]);
 
   return { board, setBoard, rowsCleared };
